60897ce661415a7e43e759a281ad5b4c5a8d267b
akshan07 <akshan@yahoo.com>
2017-06-15 16:27:42 -0700

    Add files via upload
    Udacity Class.

---
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -0,0 +1,11 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+ 
+ // $("#main").append("akshan");
+ 
+var firstName = "Aravind";
+var age = 32;
+
+var awesomeThoughts = "I am akshan and I am AWESOME!"; 
+console.log (awesomeThoughts);
